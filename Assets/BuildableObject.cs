407c8acac4e5dd779748a5c1f906584252130086
Paris <paris@octr.dev>
2024-07-20 00:03:23 +0100

    BuildableObject (Stats)

--- Assets/BuildableObject.cs
+++ Assets/BuildableObject.cs
@@ -1,16 +1,54 @@
+using Obvious.Soap;
 using UnityEngine;
+
+public enum BuildingType
+{
+    Refinery,
+    FuelingStation,
+    Drill
+}
 
 public class BuildableObject : MonoBehaviour
 {
+    [SerializeField] private BuildingType buildingType;
+
+    [Header("Building Cost")]
+    [SerializeField] private IntReference titaniumCost;
+    [SerializeField] private IntReference fuelCost;
+    [SerializeField] private IntReference DroneCost;
+
+    [Header("Building Production")]
+    [SerializeField] private IntReference titaniumProduction;
+    [SerializeField] private IntReference fuelProduction;
+    [SerializeField] private IntReference DroneProduction;
+
+    [Header("VariableReferences")]
+    [SerializeField] private IntReference currentTitanium;
+    [SerializeField] private IntReference currentFuel;
+    [SerializeField] private IntReference currentDrones;
+
+    [SerializeField] private IntReference fuelIncreaseRate;
+    [SerializeField] private IntReference fuelDecreaseRate;
+    [SerializeField] private IntReference titaniumIncreaseRate;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void OnEnable()
     {
-        
+        HandleCosts();
+        HandleProduction();
     }
 
-    // Update is called once per frame
-    void Update()
+    void HandleCosts()
     {
-        
+        currentTitanium.Value -= titaniumCost.Value;
+        currentDrones.Value -= DroneCost.Value;
+        fuelDecreaseRate.Value += fuelCost.Value;
+    }
+
+    void HandleProduction()
+    {
+        fuelIncreaseRate.Value += fuelProduction.Value;
+        titaniumIncreaseRate.Value += titaniumProduction.Value;
+        currentDrones.Value += DroneProduction.Value;
     }
 }
---
+++ Assets/_KennyGameJam/Scripts/IntVariableTicker.cs
@@ -0,0 +1,89 @@
+using Obvious.Soap;
+using UnityEngine;
+using KenneyJam.Enums;
+
+namespace KenneyJam.UI
+{
+    public class IntVariableTicker : MonoBehaviour
+    {
+        [Header("Ticker Settings")]
+        [SerializeField] private bool _isRunning = true; // To control whether the timer is running
+        [SerializeField] private TickerState tickerState;
+
+        [Header("VariableReferences")]
+        [SerializeField] private FloatReference tickRate;
+        [SerializeField] private IntReference current;
+        [SerializeField] private IntReference decreaseRate;
+        [SerializeField] private IntReference increaseRate;
+
+        [SerializeField] private FloatReference _currentTime;
+
+        void Start()
+        {
+            // Initialize the timer
+            _currentTime.Value = 0f;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (_isRunning)
+            {
+                // Decrease the timer value over time
+                _currentTime.Value += Time.deltaTime;
+                
+                if(decreaseRate > increaseRate)
+                {
+                    tickerState = TickerState.Decreasing;
+                }
+                else if(increaseRate > decreaseRate)
+                {
+                    tickerState = TickerState.Increasing;
+                }
+                else
+                {
+                    tickerState = TickerState.Idle;
+                }
+
+                if(_currentTime >= 3f)
+                {
+                    switch (tickerState)
+                    {
+                        case TickerState.Decreasing:
+                            Decrease();
+                            _currentTime.Value = 0f;
+                            break;
+                        case TickerState.Increasing:
+                            Increase();
+                            _currentTime.Value = 0f;
+                            break;
+                        case TickerState.Idle:
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+
+        // Method to update the FuelTotal value
+        private void Decrease()
+        {
+            current.Value -= decreaseRate + increaseRate;
+
+            if (current.Value <= 0)
+            {
+                GameMannager.instance.gameState = GameState.GameOver;
+                _isRunning = false;
+            }
+        }
+
+        // Method to update the FuelTotal value
+        private void Increase()
+        {
+            current.Value += increaseRate - decreaseRate;
+        }
+    }
+
+}
+
