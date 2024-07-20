600e095dd64fe0fec3016d030ccd699da9c9efa4
Paris <paris@octr.dev>
2024-07-21 00:38:26 +0100

    Scale Tween & Fixes

--- Assets/_KennyGameJam/Scripts/TweenScripts/ScaleTween.cs
+++ Assets/_KennyGameJam/Scripts/TweenScripts/ScaleTween.cs
@@ -1,21 +1,21 @@
 using DG.Tweening;
 using UnityEngine;
 
-public class PositionTween : MonoBehaviour
+public class ScaleTween : MonoBehaviour
 {
     [SerializeField] private bool _enabled;
-    [SerializeField] private Vector3 position;
+    [SerializeField] private Vector3 scale;
     [SerializeField] private float duration;
 
     [SerializeField] private bool _yoyo;
     [SerializeField] private Ease _easingType;
 
     private bool _isRunning;
-    private Vector3 _cachedPosition;
+    private Vector3 _cachedScale;
 
     private void Start()
     {
-        _cachedPosition = transform.localPosition;
+        _cachedScale = transform.localScale;
     }
 
     // Update is called once per frame
@@ -24,7 +24,7 @@ public class PositionTween : MonoBehaviour
         if(!_enabled)
         {
             _isRunning = false;
-            transform.localPosition = _cachedPosition;
+            transform.localScale = _cachedScale;
             TweenX.StopTween(transform);
             return;
         }
@@ -41,7 +41,7 @@ public class PositionTween : MonoBehaviour
             return;
         }
 
-        TweenX.TweenPosition(transform, position, duration, _yoyo, _easingType);
+        TweenX.TweenScale(transform, scale, duration, _yoyo, _easingType);
 
         _isRunning = true;
     }
--- Assets/_KennyGameJam/Scripts/_Universal Scripts/Extensions/TweenX.cs
+++ Assets/_KennyGameJam/Scripts/_Universal Scripts/Extensions/TweenX.cs
@@ -32,7 +32,7 @@ public static class TweenX
 
     public static void TweenPosition(Transform _transform, Vector3 _position, float _duration, bool _pingPong = false, Ease _ease = Ease.InOutSine)
     {
-        Vector3 targetPosition = _transform.position + _position; // Calculate the target position
+        Vector3 targetPosition = _transform.localPosition + _position; // Calculate the target position
 
         if (_pingPong)
         {
@@ -43,6 +43,21 @@ public static class TweenX
             _transform.DOLocalMove(targetPosition, _duration).SetEase(_ease);
         }
     }
+
+    public static void TweenScale(Transform _transform, Vector3 _scale, float _duration, bool _pingPong = false, Ease _ease = Ease.InOutSine)
+    {
+        Vector3 targetScale = _transform.localScale + _scale; // Calculate the target scale
+
+        if (_pingPong)
+        {
+            _transform.DOScale(targetScale, _duration).SetEase(_ease).SetLoops(-1, LoopType.Yoyo);
+        }
+        else
+        {
+            _transform.DOScale(targetScale, _duration).SetEase(_ease);
+        }
+    }
+
 
 
     public static void StopTween(Transform _transform)
