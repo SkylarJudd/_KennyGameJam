3194d8fb05be3eedb7de0a8e229bdc9e62d0a36b
SkylarJudd <136395161+SkylarJudd@users.noreply.github.com>
2024-07-21 23:16:52 +1000

    Updated Build UI to show when cant buy

--- Assets/_KennyGameJam/Scripts/_Universal Scripts/GameBehaviour.cs
+++ Assets/_KennyGameJam/Scripts/_Universal Scripts/GameBehaviour.cs
@@ -8,9 +8,12 @@ public class GameBehaviour : MonoBehaviour
     protected static ObjectPoolManager _OPM { get { return ObjectPoolManager.instance; } }
     protected static PlayerInteraction _PI { get { return PlayerInteraction.instance; } }
     protected static UIMannager _UIM { get { return UIMannager.instance; } }
-   
+    protected static SceneController _SC { get { return SceneController.instance; } }
 
-  
 
-    
+
+
+
+
+
 }
